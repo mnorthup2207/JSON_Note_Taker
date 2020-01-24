5da5f66b87cce0355bb5249ba914e9dac0c82754
McCabe Northup <mccabenorthup@gmail.com>
2020-01-23 22:19:07 -0700

    added an api routes folder and built out paths for execution

---
+++ routes/apiRoutes.js
@@ -0,0 +1,22 @@
+var noteData = require("../db/db.json");
+
+module.exports = function (app) {
+
+    app.get("/api/notes", function (req, res) {
+        res.json(noteData);
+    });
+
+    app.post("/api/notes", function (req, res) {
+        noteData.push(req.body);
+        res.json(true);
+    });
+
+    app.delete("/api/notes/:note", function (req, res) {
+        var chosen = req.params.note;
+        noteData.pop(chosen);
+        res.json(true)
+
+    })
+
+
+};
